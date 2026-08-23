24bb1a79d6e22aa922c36f32044f69fbb5af25c4
kida <isaku.kida@rootage.co.jp>
2026-08-23 15:51:31 +0900

    test

--- src/main/java/com/example/controller/G01Controller.java
+++ src/main/java/com/example/controller/G01Controller.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class G01Controller {
@@ -11,5 +14,21 @@ public class G01Controller {
 	public String getInit() {
 		return "/login/G01";
 	}
+	
+	@Autowired
+	private JdbcTemplate jdbcTemp;
+	
+	@GetMapping("/db-test")
+	@ResponseBody
+	public String dbTest() {
+		try {
+			jdbcTemp.queryForObject("select 1", Integer.class);
+			return "DB OK";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "DB ERROR:" + e.getMessage();
+		}
+	}
+	
 
 }
